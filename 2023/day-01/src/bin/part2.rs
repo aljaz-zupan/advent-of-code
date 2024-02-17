14ce952959748a6aa1c3d3e2703ab54fe12e23e8
Aljaž <aljaz.zupan@gmail.com>
2024-02-17 16:05:14 +0100

    part2 shenenigans

--- 2023/day-01/src/bin/part2.rs
+++ 2023/day-01/src/bin/part2.rs
@@ -1,11 +1,14 @@
 use std::include_str;
 
-enum Numbers {}
-
 fn main() {
     let input = include_str!("input.txt");
     let output = part2(input);
     println!("anwser: {}", output)
+}
+
+struct Number {
+    value: &str,
+    index: i16,
 }
 
 fn part2(input: &str) -> i32 {
@@ -16,13 +19,20 @@ fn part2(input: &str) -> i32 {
     ];
 
     for line in input.lines() {
-        let mut line_sum = 0;
-        let mut left = 0;
-        let mut right = 0;
+        let mut left: Number = Number {
+            value: 0,
+            index: 999,
+        };
+        let mut right: Number = Number {
+            value: 0,
+            index: -1,
+        };
 
         for &number in &numbers {
+            println!("Number: {}", &number);
             if let Some(first_index) = line.find(&number) {
-                match left {
+                println!("first_index {}", first_index);
+                match left.value {
                     0 => {
                         left = return_number(&number);
                         right = return_number(&number)
@@ -30,13 +40,16 @@ fn part2(input: &str) -> i32 {
                     _ => left = return_number(&number),
                 }
             } else if let Some(last_index) = line.rfind(&number) {
-                match right {
+                println!("last_index {}", last_index);
+                match right.value {
                     0 => right = return_number(&number),
+                    _ => {}
                 }
             }
         }
+        println!("{} {} {}", &line, &left, &right);
         let line_string = format!("{}{}", &left.to_string(), &right.to_string());
-        line_sum = line_string.parse().unwrap();
+        let line_sum: i32 = line_string.parse().unwrap();
         sum += line_sum
     }
     sum
@@ -65,6 +78,6 @@ mod tests {
         let test_string = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen";
 
         let result = super::part2(test_string);
-        assert_eq!(result, 142);
+        assert_eq!(result, 281);
     }
 }
