849404f4950ceeb943d4b26f01b5039648c16816
aljaz-zupan <aljaz.zupan@gmail.com>
2024-02-17 13:29:51 +0100

    added part2 and return number

--- 2023/day-01/src/bin/part2.rs
+++ 2023/day-01/src/bin/part2.rs
@@ -1,8 +1,6 @@
 use std::include_str;
 
-enum Numbers {
-
-}
+enum Numbers {}
 
 fn main() {
     let input = include_str!("input.txt");
@@ -12,35 +10,61 @@ fn main() {
 
 fn part2(input: &str) -> i32 {
     let mut sum = 0;
-    let numbers = vec!["1","2","3","4","5","6","7","8","9","one","two","three","four","five","six","seven","eight","nine"]
+    let numbers = vec![
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six",
+        "seven", "eight", "nine",
+    ];
 
     for line in input.lines() {
         let mut line_sum = 0;
         let mut left = 0;
         let mut right = 0;
-        
-        
-        for &number in &numbers {
 
+        for &number in &numbers {
             if let Some(first_index) = line.find(&number) {
                 match left {
-                    0 => {left = &number, right = &number},
-                    _ => left = &number
+                    0 => {
+                        left = return_number(&number);
+                        right = return_number(&number)
+                    }
+                    _ => left = return_number(&number),
                 }
-
+            } else if let Some(last_index) = line.rfind(&number) {
+                match right {
+                    0 => right = return_number(&number),
+                }
             }
-        }    
+        }
+        let line_string = format!("{}{}", &left.to_string(), &right.to_string());
+        line_sum = line_string.parse().unwrap();
+        sum += line_sum
     }
     sum
+}
+
+fn return_number(number: &str) -> i32 {
+    let the_number = match number {
+        "1" | "one" => 1,
+        "2" | "two" => 2,
+        "3" | "three" => 3,
+        "4" | "four" => 4,
+        "5" | "five" => 5,
+        "6" | "six" => 6,
+        "7" | "seven" => 7,
+        "8" | "eight" => 8,
+        "9" | "nine" => 9,
+        _ => 0,
+    };
+    the_number
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn test_part2() {
-        let test_string = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet";
+        let test_string = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen";
 
-        let result = super::part1(test_string);
+        let result = super::part2(test_string);
         assert_eq!(result, 142);
     }
 }
