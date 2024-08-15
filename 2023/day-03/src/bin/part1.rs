6a75152c1e57a549a86ce06fb35c8d4d58fc5d84
Aljaž <aljaz.zupan@gmail.com>
2024-08-15 23:27:37 +0200

    check indexes

--- 2023/day-03/src/bin/part1.rs
+++ 2023/day-03/src/bin/part1.rs
@@ -1,13 +1,27 @@
+use std::i32;
+
+struct Symbol_neighbours {
+    symbol: char,
+    symbol_index: usize,
+    current_line: String,
+    next_line: Option<String>,
+    previous_line: Option<String>,
+}
+
 fn main() {
     let input = include_str!("input.txt");
     let symbols: &[_] = &['+', '*', '%', '/', '@', '#', '-', '$', '&', '='];
 
-    for (line_number, line) in input.lines().into_iter().enumerate() {
-        for (char_index, char) in line.chars().enumerate() {
-            if symbols.contains(&char) {
-                print!("{} at {}, ", char, char_index);
-            }
+    let mut file = input.lines().into_iter().enumerate();
+
+    for (line_index, line) in file {
+        let index = i32::try_from(line_index).unwrap();
+        let max = i32::try_from(file.copy.count()).unwrap() - 1;
+        // If first line then just continue since there are no symbols on firs line
+        if (index == 0 || index == max) {
+            continue;
+        } else {
+            println!("{}", &line);
         }
-        print!("\n");
     }
 }
