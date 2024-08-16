b2d027bc7b28cc80cc8c62cf786844acc31a8433
Aljaž <aljaz.zupan@gmail.com>
2024-08-16 23:18:48 +0200

    fix another borrow cheker

--- 2023/day-03/src/bin/part1.rs
+++ 2023/day-03/src/bin/part1.rs
@@ -1,6 +1,4 @@
-use std::iter::Enumerate;
-
-struct Symbol_neighbours {
+struct SymbolNeighbours {
     symbol: char,
     symbol_index: usize,
     current_line: String,
@@ -16,7 +14,6 @@ fn main() {
     let max: i32 = i32::try_from(file.clone().count()).unwrap() - 1;
 
     for (line_index, line) in file {
-        print!("{} ->", line_index);
         //let index = i32::try_from(line_index).unwrap();
 
         // If first line then just continue since there are no symbols on firs line
@@ -24,9 +21,10 @@ fn main() {
         {
             continue;
         } else {
+            print!("{} ->", &line_index);
             //println!("{}: {}", &line_index, &line);
             for (char_index, char) in line.chars().enumerate() {
-                if (symbols.contains(&char)) {
+                if symbols.contains(&char) {
                     print!("{}[{}], ", char, char_index);
                 }
             }
@@ -34,3 +32,7 @@ fn main() {
         println!("");
     }
 }
+
+/*fn top_line(line: &str, index: usize): Option<Vec<i32>> {
+
+}*/
