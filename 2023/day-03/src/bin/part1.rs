08cc3e2b5a3f19918a479cfa0b52bf8824487c76
Aljaž <aljaz.zupan@gmail.com>
2024-08-17 17:24:13 +0200

    borrow checker shenenigans #1

--- 2023/day-03/src/bin/part1.rs
+++ 2023/day-03/src/bin/part1.rs
@@ -10,14 +10,15 @@ fn main() {
     let input: &str = include_str!("input.txt");
     let symbols: Vec<char> = vec!['+', '*', '%', '/', '@', '#', '-', '$', '&', '='];
 
-    let file = input.lines().enumerate();
+    let mut file = input.lines().enumerate();
     let max: i32 = i32::try_from(file.clone().count()).unwrap() - 1;
 
     for (line_index, line) in file {
         //let index = i32::try_from(line_index).unwrap();
 
         // If first line then just continue since there are no symbols on firs line
-        if line_index == usize::try_from(0).unwrap() || line_index == usize::try_from(max).unwrap()
+        if (line_index == usize::try_from(0).unwrap()
+            || line_index == usize::try_from(max).unwrap())
         {
             continue;
         } else {
@@ -25,7 +26,12 @@ fn main() {
             //println!("{}: {}", &line_index, &line);
             for (char_index, char) in line.chars().enumerate() {
                 if symbols.contains(&char) {
-                    print!("{}[{}], ", char, char_index);
+                    let top = &file.clone().nth(&line_index - 1);
+                    let bottom = &file.clone().nth(&line_index + 1);
+                    print!(
+                        "{}[{}], top: {:?}, bottom:{:?} , ",
+                        char, char_index, top, bottom
+                    );
                 }
             }
         }
