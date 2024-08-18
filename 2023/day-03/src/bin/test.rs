5be909a709269e5f5dfadc29480f09c51606d62d
Aljaž <aljaz.zupan@gmail.com>
2024-08-18 17:07:31 +0200

    test Digits #1

--- 2023/day-03/src/bin/test.rs
+++ 2023/day-03/src/bin/test.rs
@@ -1,3 +1,13 @@
+struct Digit {
+    current_index: usize,    // Current index of the last found char of a digit
+    digit_string: Vec<char>, // Add char to string if is next to previus char
+    direction: Direction,    // Current dirrection of itterator
+}
+enum Direction {
+    Left,
+    Right,
+}
+
 fn main() {
     let numbers = vec!['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     let string: String = "....573.613.........965............691......892..948.......964........439.375..................320......273...........352.284...............".to_string();
