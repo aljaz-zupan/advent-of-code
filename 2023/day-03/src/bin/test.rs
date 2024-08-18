ae83cab21f72f0a8ed2afa7e6f1f3d8ce23399d0
aljaz-zupan <aljaz.zupan@gmail.com>
2024-08-18 20:16:28 +0200

    created a digit finder

--- 2023/day-03/src/bin/test.rs
+++ 2023/day-03/src/bin/test.rs
@@ -12,11 +12,21 @@ fn main() {
     let numbers = vec!['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     let string: String = "....573.613.........965............691......892..948.......964........439.375..................320......273...........352.284...............".to_string();
 
-    let mut num: Vec<char> = vec![];
+    let mut num: Vec<String> = vec![];
+    let mut temp: Vec<char> = vec![];
 
     for (index, char) in string.chars().enumerate() {
         if numbers.contains(&char) {
-            println!("[{}]{}", index, char);
+            temp.push(char.clone());
+        } else {
+            if !temp.is_empty() {
+                let collection: String = temp.iter().collect();
+                num.push(collection);
+                println!("[{}]{}", index, char.clone());
+            }
+            temp.clear();
         }
     }
+
+    println!("num: {:?}", num)
 }
